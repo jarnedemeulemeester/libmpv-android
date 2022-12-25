8961881dda3c687ee9efec55a116ae9de1f7f667
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-12-25 16:25:27 +0100

    Disable vulkan in libplacebo

--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -13,25 +13,9 @@ else
 	exit 255
 fi
 
-# Android provides Vulkan, but no pkgconfig file
-# you can double-check the version in vk.xml (ctrl+f VK_API_VERSION)
-mkdir -p "$prefix_dir"/lib/pkgconfig
-cat >"$prefix_dir"/lib/pkgconfig/vulkan.pc <<"END"
-Name: Vulkan
-Description:
-Version: 1.2.0
-Libs: -lvulkan
-Cflags:
-END
-
-ndk_vulkan="$(dirname "$(which ndk-build)")/sources/third_party/vulkan/src"
-
 unset CC CXX
-# use third party over system headers (which are incomplete)
-export CFLAGS="-isystem $ndk_vulkan/include"
 meson $build --cross-file "$prefix_dir"/crossfile.txt \
-  -Dvulkan-registry="$ndk_vulkan/registry/vk.xml" -Ddemos=false
-#	-Dvulkan=disabled -Db_lto=true -Dtests=false -Dglslang=disabled -Dd3d11=disabled -Dlcms=disabled -Ddemos=false
+  -Dvulkan=disabled -Ddemos=false
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
