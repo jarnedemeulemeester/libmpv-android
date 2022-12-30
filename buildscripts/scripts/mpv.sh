303e609658bb0d1b167d43038b324ba9f0cbc14c
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-12-30 22:38:23 +0100

    Revert "Switch to meson for mpv build"
    This reverts commit bc1952296f34e631d71c85b9e34454e998049b0d.

--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -2,30 +2,27 @@
 
 . ../../include/path.sh
 
-build=_build$ndk_suffix
-
 if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
-	rm -rf $build
+	rm -rf _build$ndk_suffix
 	exit 0
 else
 	exit 255
 fi
 
-unset CC CXX # meson wants these unset
+[ -f waf ] || ./bootstrap.py
 
-meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-  -Ddefault_library=shared \
-  -Dcplayer=false \
-  -Dlibmpv=true \
-  -Dvulkan=disabled \
-  -Diconv=disabled \
-  -Dlua=lua \
-  -Djpeg=disabled
+PKG_CONFIG="pkg-config --static" \
+./waf configure \
+    --disable-cplayer \
+    --disable-vulkan \
+	--disable-iconv --lua=52 \
+	--enable-libmpv-shared \
+	--disable-manpage-build \
+	-o "`pwd`/_build$ndk_suffix"
 
-meson compile -C $build -j$cores
-
-DESTDIR="$prefix_dir" meson install -C $build
+./waf build -j$cores
+./waf install --destdir="$prefix_dir"
 
 ln -sf "$prefix_dir"/lib/libmpv.so "$native_dir"
