e339a68743cdc666533363db60ec0128fabfa630
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-05-07 17:30:26 +0200

    build: upgrade ndk, build tools, libass, harfbuzz, mbedtls, libplacebo, dav1d, ffmpeg

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -3,18 +3,18 @@
 ## Dependency versions
 
 v_sdk=9123335_latest
-v_ndk=25.1.8937393
-v_sdk_build_tools=33.0.1
+v_ndk=25.2.9519653
+v_sdk_build_tools=33.0.2
 
 v_lua=5.2.4
-v_libass=0.17.0
-v_harfbuzz=7.0.0
+v_libass=0.17.1
+v_harfbuzz=7.2.0
 v_fribidi=1.0.12
 v_freetype=2-13-0
-v_mbedtls=3.3.0
-v_libplacebo=5.229.2
-v_dav1d=1.1.0
-v_ffmpeg=5.1.2
+v_mbedtls=3.4.0
+v_libplacebo=5.264.1
+v_dav1d=1.2.0
+v_ffmpeg=6.0
 v_mpv=0.35.1
 
 
