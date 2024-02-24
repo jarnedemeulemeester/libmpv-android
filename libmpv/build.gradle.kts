56bc62c8a9c755f32053ae7b2ff3fca61314f1c1
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-02-24 22:48:43 +0100

    build: bump version to 0.2.0

--- libmpv/build.gradle.kts
+++ libmpv/build.gradle.kts
@@ -45,7 +45,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "dev.jdtech.mpv"
             artifactId = "libmpv"
-            version = "0.1.4"
+            version = "0.2.0"
 
             afterEvaluate {
                 from(components["release"])
