d8d0abaf6b86ad050e74b967c205a5d4d78107d8
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-06-30 13:45:14 +0200

    chore(deps): update harfbuzz and dav1d
    harfbuzz 8.5.0 -> 9.0.0
    dav1d 1.4.2 -> 1.4.3

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -8,12 +8,12 @@ v_sdk_build_tools=34.0.0
 
 v_lua=5.2.4
 v_libass=0.17.2
-v_harfbuzz=8.5.0
+v_harfbuzz=9.0.0
 v_fribidi=1.0.15
 v_freetype=2-13-2
 v_mbedtls=3.5.2
 v_libplacebo=6.338.2
-v_dav1d=1.4.2
+v_dav1d=1.4.3
 v_ffmpeg=7.0.1
 v_mpv=0.38.0
 
