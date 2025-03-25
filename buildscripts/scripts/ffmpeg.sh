46a65fc9a43b359aa5f788fd34a58a6665fa18a7
XBigTK13X <xbigtk13x@gmail.com>
2025-03-24 21:15:16 -0500

    Enable filters and make cleanup of observers easier

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -30,7 +30,7 @@ cpuflags=
 	--enable-{jni,mediacodec,mbedtls,libdav1d} --disable-vulkan \
 	--disable-static --enable-shared --enable-{gpl,version3} \
 	--disable-{stripping,doc,programs} \
-	--disable-{muxers,encoders,devices,filters} \
+	--disable-{muxers,encoders,devices} \
 	--disable-v4l2-m2m
 
 make -j$cores
