c04e3c6bfa35ef387478830abac13b960e4776cf
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-07-11 16:23:38 +0200

    ci: switch to `com.vanniktech.maven.publish` for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,11 @@
 plugins {
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.publish.plugin) apply true
+    alias(libs.plugins.maven.publish) apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
         google()
-    }
-}
-
-// Create variables with empty default values
-ext["signing.keyId"] = null
-ext["signing.password"] = null
-ext["signing.key"] = null
-ext["ossrhUsername"] = null
-ext["ossrhPassword"] = null
-ext["sonatypeStagingProfileId"] = null
-
-val secretPropsFile: File = project.rootProject.file("local.properties")
-if (secretPropsFile.exists()) {
-    // Read local.properties file first if it exists
-    secretPropsFile.reader().use {
-        java.util.Properties().apply {
-            load(it)
-        }
-    }.onEach { (name, value) -> 
-        ext[name.toString()] = value
-    }
-} else {
-    // Use system environment variables
-    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
-    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
-    ext["sonatypeStagingProfileId"] = System.getenv("SONATYPE_STAGING_PROFILE_ID")
-    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
-    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
-    ext["signing.key"] = System.getenv("SIGNING_KEY")
-}
-
-fun getExtraString(name: String) = ext[name]?.toString()
-
-nexusPublishing {
-    this.repositories {
-        sonatype {
-            stagingProfileId = getExtraString("sonatypeStagingProfileId")
-            username = getExtraString("ossrhUsername")
-            password = getExtraString("ossrhPassword")
-            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
-            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        }
     }
 }
--- libmpv/build.gradle.kts
+++ libmpv/build.gradle.kts
@@ -1,7 +1,8 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+
 plugins {
     alias(libs.plugins.android.library)
-    `maven-publish`
-    signing
+    alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -30,64 +31,56 @@ android {
             version = "4.0.2"
         }
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
 }
 
 dependencies {
     implementation(libs.androidx.annotation)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "dev.jdtech.mpv"
-            artifactId = "libmpv"
-            version = "0.4.1"
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
-            }
+    configure(
+        platform = AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = false,
+        )
+    )
 
-            pom {
-                name = "dev.jdtech.mpv"
-                description = "libmpv for Android"
-                url = "https://github.com/jarnedemeulemeester/libmpv-android"
-                licenses {
-                    license {
-                        name = "MIT license"
-                        url = "https://github.com/jarnedemeulemeester/libmpv-android/blob/main/LICENSE"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "jarnedemeulemeester"
-                        name = "Jarne Demeulemeester"
-                        email = "jarnedemeulemeester@gmail.com"
-                    }
-                }
-                scm {
-                    url = "https://github.com/jarnedemeulemeester/libmpv-android.git"
-                    connection = "scm:git@github.com:jarnedemeulemeester/libmpv-android.git"
-                    developerConnection = "scm:git@github.com:jarnedemeulemeester/libmpv-android.git"
-                }
-                issueManagement {
-                    url = "https://github.com/jarnedemeulemeester/libmpv-android/issues"
-                }
+    coordinates(
+        groupId = "dev.jdtech.mpv",
+        artifactId = "libmpv",
+        version = "0.4.1"
+    )
+
+    pom {
+        name = "libmpv-android"
+        description = "libmpv for Android"
+        inceptionYear = "2023"
+        url = "https://github.com/jarnedemeulemeester/libmpv-android"
+        licenses {
+            license {
+                name = "MIT license"
+                url = "https://github.com/jarnedemeulemeester/libmpv-android/blob/main/LICENSE"
             }
         }
+        developers {
+            developer {
+                id = "jarnedemeulemeester"
+                name = "Jarne Demeulemeester"
+                email = "jarnedemeulemeester@gmail.com"
+            }
+        }
+        scm {
+            url = "https://github.com/jarnedemeulemeester/libmpv-android.git"
+            connection = "scm:git@github.com:jarnedemeulemeester/libmpv-android.git"
+            developerConnection = "scm:git@github.com:jarnedemeulemeester/libmpv-android.git"
+        }
+        issueManagement {
+            system = "GitHub"
+            url = "https://github.com/jarnedemeulemeester/libmpv-android/issues"
+        }
     }
-}
-
-signing {
-    useInMemoryPgpKeys(
-        rootProject.ext["signing.keyId"]?.toString(),
-        rootProject.ext["signing.key"]?.toString(),
-        rootProject.ext["signing.password"]?.toString(),
-    )
-    sign(publishing.publications)
 }
